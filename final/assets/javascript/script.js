6cbf21e12822e696770a9c439c7f4fbb9110db4b
katyhonguyen <katynguyen@berkeley.edu>
2024-04-29 01:56:51 -0700

    finished final

---
+++ final/assets/javascript/script.js
@@ -0,0 +1,8 @@
+// script.js
+function changeColor(element) {
+    if (element.style.backgroundColor === 'blue') {
+        element.style.backgroundColor = 'red';
+    } else {
+        element.style.backgroundColor = 'blue';
+    }
+}
---
+++ final/script.js
@@ -0,0 +1,8 @@
+// script.js
+function changeColor(element) {
+    if (element.style.backgroundColor === '#DADADA' || element.style.backgroundColor === '') {
+        element.style.backgroundColor = '#BDA7F9';
+    } else {
+        element.style.backgroundColor = '#DADADA';
+    }
+}
